6ca841c5c249310542608539bb397a67e6cab27c
Tyler Long <tyler4long@gmail.com>
2012-04-09 08:08:12 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails'
+gem 'jquery-rails'
+gem 'pg'
+gem 'bootstrap-sass'
+gem 'bootswatch-rails'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
+group :development, :test do
+  gem 'annotate', '~> 2.4.1.beta'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'spork'
+  gem 'guard-rspec'
+  gem 'guard-spork'
+  gem 'rb-inotify'
+  gem 'libnotify'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EverblogRails::Application.config.secret_token = 'ce1d457e26bbf0430e95f764e038f5a5ca23836a3433b6e39bea2e7799801e94914044e197db904a864360ccabdcbed53ef0e12731649634a280768879b6d456'
