62cfb5d0401f701facea070cce2a676cdb83c67c
Tyler Long <tyler4long@gmail.com>
2012-04-12 22:00:53 -0700

    First public version

--- app/controllers/evernotes_controller.rb
+++ app/controllers/evernotes_controller.rb
@@ -12,17 +12,11 @@ class EvernotesController < ApplicationController
   private
 
     def authenticate
-      user_store_url = "https://sandbox.evernote.com/edam/user"
-      config = {
-         :username => 'tylerlong',
-         :password => 'fihu2zim',
-         :consumer_key => 'tylerlong',
-         :consumer_secret => 'c9a682d60f9a442c'
-      }
-      user_store = Evernote::UserStore.new(user_store_url, config)
+      user_store_url = "https://#{CONFIG['evernote_domain']}/edam/user"
+      user_store = Evernote::UserStore.new(user_store_url, CONFIG['evernote_config'])
       authentication = user_store.authenticate
       user, auth_token = authentication.user, authentication.authenticationToken
-      note_store_url = "http://sandbox.evernote.com/edam/note/#{user.shardId}"
+      note_store_url = "http://#{CONFIG['evernote_domain']}/edam/note/#{user.shardId}"
       note_store = Evernote::NoteStore.new(note_store_url)
       return note_store, auth_token
     end
@@ -35,8 +29,11 @@ class EvernotesController < ApplicationController
       note_store.findNotes(auth_token, Evernote::EDAM::NoteStore::NoteFilter.new(notebookGuid: notebook.guid), 0, 1000).notes
     end
 
+    CONTENT_REGEXP = /<en-note>(.+?)<\/en-note>/m
     def get_note(guid)
       note_store, auth_token = authenticate
-      note_store.getNote(auth_token, guid, true, true, true, true)
+      note = note_store.getNote(auth_token, guid, true, true, true, true)
+      CONTENT_REGEXP =~ note.content
+      { title: note.title, content: $1 }
     end
 end
