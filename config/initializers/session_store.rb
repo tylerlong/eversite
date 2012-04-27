8aa60dfe9731c5ed0ed9d7ab07f40206672d3176
Tyler Long <tyler4long@gmail.com>
2012-04-27 08:49:40 -0700

    Change footer and configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-EverblogRails::Application.config.session_store :cookie_store, key: '_everblog_rails_session'
+EverblogRails::Application.config.session_store :cookie_store, key: '_eversite_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
